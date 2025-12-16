fd6d94400533fd503c39b1d6affe7f7183ddca78
piy-771 <piyushsahu2103@gmail.com>
2025-12-17 01:28:30 +0530

    Capthcha Validation

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
+let captchchecked = false;
+function beforesubmit(event){
+    if(captchchecked){
+         let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate.value);  //string --> date(en_IN)
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formattedDate;
+    }else{
+        alert("Please check the reCAPTCHA box to submit the lead");
+        event.preventDefault();
+    }
+   
 }
 
  function timestamp() {
@@ -16,3 +23,8 @@ function beforesubmit(){
         elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     } } setInterval(timestamp, 500); 
+
+
+    function capthchasuccess(){
+        captchchecked = true;
+    }
